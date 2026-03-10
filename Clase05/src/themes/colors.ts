661bb3981b098bb5a8e7c83db13606e38daabedb
Apesca <abrahampesca666@gmail.com>
2026-03-10 02:39:22 -0500

    Correcion de clase 5

---
+++ Clase05/src/themes/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  primary: "#6366F1",      
+  secondary: "#8B5CF6",    
+  background: "#F8FAFC",   
+  white: "#FFFFFF",
+  black: "#0F172A",        
+  gray: "#64748B",         
+  success: "#22C55E",      
+  danger: "#EF4444",       
+  warning: "#F59E0B",      
+}
