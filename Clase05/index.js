654343ae895a66a3c60a28154aba4e6fb686c851
Apesca <abrahampesca666@gmail.com>
2026-04-17 18:40:18 -0500

    feat: calculadora con hooks y atomic design

--- Clase05/index.js
+++ Clase05/index.js
@@ -6,6 +6,5 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 
-const myApp = new App().render;
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => new App().render);
---
+++ usandoHooks/src/themes/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  primary: "#111827",
+  secondary: "#374151",
+  background: "#F9FAFB",
+  white: "#FFFFFF",
+  black: "#111827",
+  gray: "#6B7280",
+  success: "#16A34A",
+  danger: "#DC2626",
+  warning: "#F59E0B",
+}
