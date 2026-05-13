644501ccec8088b840499165d28aac9bdea32e5d
Abraham Pesca Prada <apescap@MacBook-Air-de-Abraham.local>
2026-05-13 16:12:06 -0500

    feat: update navigation theme and src

--- navigation/src/components/organisms/LoginForm/LoginForm.tsx
+++ navigation/src/components/organisms/LoginForm/LoginForm.tsx
@@ -1,20 +1,67 @@
 import React from "react";
-import { View, Text } from "react-native";
+
+import { View, Text, TouchableOpacity } from "react-native";
+
+// Importamos atoms
 import { Button } from "../../atoms";
 
+import { styles } from "./LoginFormStyles"; 
+
+// Importamos molecules
+import {
+  InputField,
+} from "../../moleculas";
+
+// Props del formulario
 interface LoginProps {
+
+  // Función del botón login
   onSubmit?: () => void;
 }
 
-const LoginForm = ({ onSubmit }: LoginProps) => {
+// Organism LoginForm
+const LoginForm = ({
+  onSubmit,
+}: LoginProps) => {
+
   return (
     <View>
-      <Text>Formulario Login</Text>
 
-      <Button
-        title="Iniciar Sesión"
-        onSubmit={onSubmit}
+      
+      {/* Campo email */}
+      <InputField
+        label="Username"
+        placeholder="Enter your email"
       />
+
+      
+
+      {/* Campo contraseña */}
+      <InputField
+        label="Password"
+        placeholder="Enter your password"
+        secureTextEntry
+      />
+      
+      {/* Forgot password SIN FUNCION TODAVIAAAAAAAAAAAAAAAAAAAA*/}
+      <TouchableOpacity>
+        <Text style={styles.forgotPassword}>FORGOT PASSWORD?</Text>
+      </TouchableOpacity>
+
+
+      {/* Boton login SIN FUNCION TODAVIAAAAAAAAAAAAAAAAAAAA*/}
+      <Button
+        title="LOGIN"
+      />
+
+      {/* Footer: Sign up */}
+      <View style={styles.footerContainer}>
+        <Text style={styles.footerText}>Don't have an account?</Text>
+        <TouchableOpacity onPress={onSubmit}>
+          <Text style={styles.signUpText}>SIGN UP</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   );
 };
---
+++ navigation/src/themes/colors.ts
@@ -0,0 +1,15 @@
+export const colors = {
+  primary: "#0F172A",
+  secondary: "#1E293B",
+
+  white: "#e9e7e7",
+  black: "#020617",
+
+  gray100: "#F8FAFC",
+  gray200: "#E2E8F0",
+  gray300: "#94A3B8",
+  gray400: "#475569",
+
+  success: "#10B981",
+  error: "#F43F5E",
+};
